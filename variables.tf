82e972a61bfe03c5764bc1d3106addc799bc48b8
JeanAlvarado <jean.alvarado_consultor@mdp.com.pe>
2022-03-30 20:44:45 -0500

    vpc terraform change name

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "networking" {
   })
   default = {
    cidr_block       = "10.0.0.0/16"
-   vpc_name         = "terraform-vpc"
+   vpc_name         = "jalvarado-vpc"
    azs              = ["us-east-1a", "us-east-1b"]
    public_subnets   = ["10.0.1.0/24", "10.0.2.0/24"]
    private_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
